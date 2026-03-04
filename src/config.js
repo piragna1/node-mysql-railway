10236505e56aec0519310da7ce26c2a59c35cbfb
piragna <gonzalovarelaalagna@gmail.com>
2026-03-04 09:20:55 -0300

    cambios

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "";
-export const DB_NAME = process.env.DB_NAME || "usersdb";
-export const DB_PORT = process.env.PORT || 3306;
+export const MYSQLHOST = process.env.MYSQLHOST || "localhost";
+export const MYSQLUSER = process.env.MYSQLUSER || "root";
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || "";
+export const MYSQLNAME = process.env.MYSQLNAME || "usersdb";
+export const MYSQLPORT = process.env.MYSQLPORT || 3306;
--- src/db.js
+++ src/db.js
@@ -1,10 +1,16 @@
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "./config.js";
+import {
+  MYSQLHOST,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLNAME,
+  MYSQLPORT,
+} from "./config.js";
 
 import { createPool } from "mysql2/promise";
 export const pool = createPool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_NAME,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  host: MYSQLHOST,
+  port: MYSQLPORT,
+  database: MYSQLNAME,
 });
