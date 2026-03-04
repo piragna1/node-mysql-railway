0922aeb14c7353db017eac0c270ea51b02eac1fe
piragna <gonzalovarelaalagna@gmail.com>
2026-03-04 08:32:40 -0300

    2nd commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import express from "express";
 import { pool } from "./db.js";
+import { PORT } from "./config.js";
 const app = express();
-const PORT = process.env.PORT ?? 3000;
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   console.log("request received");
+  const [rows] = await pool.query("Select * from users;");
+  res.json(rows);
   res.end("Welcome to server");
 });
 app.get("/ping", async (req, res) => {
   console.log("request received");
   const [result] = await pool.query(`Select "hello world" as Result`);
   res.json(result[0]);
+});
+app.get("/create", async (req, res) => {
+  const result = await pool.query('insert into users(name) values ("John")');
+  res.json(result);
 });
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "./config.js";
+
 import { createPool } from "mysql2/promise";
 export const pool = createPool({
-  user: "root",
-  password: "",
-  host: "localhost",
-  port: 3306,
-  database: "usersdb",
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
 });
